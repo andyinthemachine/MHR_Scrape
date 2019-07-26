8f9762972accbfd00f9056ab5ec0bc424cb50c1e
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-07-25 20:05:43 -0600

    comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,53 @@
 
 
 
-$.getJSON("/articles", function(data) {
-  for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].summary + "</p>");
-  }
+
+$.getJSON("/articles", function (data) {
+
+  $article_list = $("#mhr-articles");
+  console.log(data);
+
+  const $articles = data.map(function (article) {
+    const $new_article = $('<h4>').text(article.title).append(
+      $('<p>').text(article.summary),
+      $("<button>").attr({ class: "note-button", "data-id": article._id }).text(`add comment`)
+    );
+    return $new_article;
+  });
+  $article_list.append($articles);
 });
 
 
-$(document).on("click", "p", function() {
-  $("#notes").empty();
+
+$("#mhr-articles").on("click", ".note-button", function () {
+  $("#mhr-notes").empty();
   var thisId = $(this).attr("data-id");
 
-  $.ajax({method: "GET",url: "/articles/" + thisId}).then(function(data) {
-      console.log(data);
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      $("#notes").append("<input id='titleinput' name='title' >");
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+  $.ajax({ method: "GET", url: "/articles/" + thisId }).then(function (data) {
+    $("#mhr-notes").append("<h2>" + data.title + "</h2>");
+    $("#mhr-notes").append("<input placeholder='name' id='titleinput' name='title' >");
+    $("#mhr-notes").append("<textarea placeholder='comment' id='bodyinput' name='body'></textarea>");
+    $("#mhr-notes").append("<button data-id='" + data._id + "' id='savenote'>Save Comment</button>");
+    $("#mhr-notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Comment</button>");
 
-      if (data.note) {
-        $("#titleinput").val(data.note.title);
-        $("#bodyinput").val(data.note.body);
-      }
-    });
+    if (data.note) {
+      $("#titleinput").val(data.note.title);
+      $("#bodyinput").val(data.note.body);
+    }
+  });
 });
 
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
   var thisId = $(this).attr("data-id");
 
-  $.ajax({method: "POST", url: "/articles/" + thisId, data: {
-    title: $("#titleinput").val(),
+  $.ajax({
+    method: "POST", url: "/articles/" + thisId, data: {
+      title: $("#titleinput").val(),
       body: $("#bodyinput").val()
     }
-  }).then(function(data) {
-      console.log(data);
-      $("#notes").empty();
-    });
+  }).then(function (data) {
+    $("#mhr-notes").empty();
+  });
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
